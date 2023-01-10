86a7a68c86f9bbeb153838a811a7a22489daa92e
Giovanni Medrano <giovannimedrano@Giovannis-MBP.hitronhub.home>
2023-01-10 15:23:46 -0600

    variables, data types

---
+++ 02_variables_and_data_types/script.js
@@ -0,0 +1,27 @@
+const variableName1 = "welcome to variables"; //doesnt change ever
+let variableName = "welcome to variables"; //can change
+
+variableName = "hello there"
+console.log(variableName + "  oink");
+
+
+// Creating a variable using var keyword
+var variable1 = "test";
+
+// Creating a variable using let keyword
+let variable2 = "test";
+
+// use command forward slash to comment an entire line
+// Hover several lines and use command forward slash to comment them all
+
+// Creating a variable using const keyword
+const variable3 = "test";
+
+// Variable naming rules to follow
+// 1. The name should be unique
+// 2.  The name should not be any reserved key words
+// 3. The name should start with a character, _, or a $ sign.
+
+// String DATA type
+const exampleString = "Hello, world!";
+console.log(exampleString);
